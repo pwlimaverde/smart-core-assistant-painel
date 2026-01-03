1af56a134b8156fa2133a91459de138c620cc326
pwlimaverde <pwlimaverde@gmail.com>
2026-01-03 20:38:35 -0300

    feat: configuracao servidor de email

--- src/smart_core_assistant_painel/app/ui/core/settings.py
+++ src/smart_core_assistant_painel/app/ui/core/settings.py
@@ -234,6 +234,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Email Configuration (SMTP)
+# https://docs.djangoproject.com/en/5.2/topics/email/
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST", default="smtp-relay.brevo.com")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="suporte@smartcoreassistant.com.br")
+SERVER_EMAIL = DEFAULT_FROM_EMAIL  # Para erros 500
+
+
 # Auth Redirects
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
